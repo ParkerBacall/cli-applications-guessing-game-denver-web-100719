91a3f86b7ff8048f8bd99b9d172aca92d86c3e25
Parker Bacall <pbacall2@gmail.com>
2019-11-12 03:44:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,13 +4,13 @@ def run_guessing_game
   number = rand(6)+1
   input = gets.chomp
 
+  if input == number
+    puts "You guessed the correct number!"
+  end
+
 
   if input != number
     puts "Sorry! The computer guessed #{number}."
-  end
-
-  if input == number
-    puts "You guessed the correct number!"
   end
 
   if input == 'exit'
